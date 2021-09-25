23414f4d3eb19b6230dc64ae8ea4b4e9c1641599
franzua <edson.andrez@outlook.es>
2021-09-24 23:49:15 -0600

    Estadisticas view.

--- app/Http/Controllers/EstadisticasController.php
+++ app/Http/Controllers/EstadisticasController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Comercio;
 use App\Departamento;
 use App\Municipio;
+use App\Queja;
 use App\Region;
 use Illuminate\Http\Request;
 
@@ -22,6 +23,7 @@ class EstadisticasController extends Controller
         $municipios = Municipio::all();
         $comercios = Comercio::all();
 
+
         return view('estadisticas.index',
             [
                 'regiones' => $regiones,
@@ -29,5 +31,24 @@ class EstadisticasController extends Controller
                 'municipios' => $municipios,
                 'comercios' => $comercios
             ]);
+    }
+
+
+    public function ajax(Request $request)
+    {
+
+        $id_region = $request->get('id_region') == null ? '' : $request->get('id_region');
+        $id_departamento = $request->get('id_departamento') == null ? '' : $request->get('id_departamento');
+        $id_municipio = $request->get('id_municipio') == null ? '' : $request->get('id_municipio');
+        $id_comercio = $request->get('id_comercio') == null ? '' : $request->get('id_comercio');
+
+        $regiones = Region::all();
+
+        $departamentos = Departamento::all();
+        $municipios = Municipio::all();
+        $comercios = Comercio::all();
+
+        $quejas = Queja::where();
+
     }
 }
